17ccc72b026cacf5d480359680e4d82e3ab5dcba
juanfranbrv <juanfranbrv@gmail.com>
2024-11-28 00:18:47 +0100

    con groq

--- pages/transcription.py
+++ pages/transcription.py
@@ -1,21 +1,51 @@
+# ════════════════════════════════════════════════════════════════════
+# IMPORTAMOS LAS LIBRERIAA
+# ════════════════════════════════════════════════════════════════════
 import streamlit as st
-import openai
+from groq import Groq
 
-key = st.secrets["OPENAI_API_KEY"]
+
+# ════════════════════════════════════════════════════════════════════
+#      RECUPERAR LA CLAVE DE API DE LAS VARIABLES DEL FICHERO .env
+# ════════════════════════════════════════════════════════════════════
+# Recuperar la clave de API de las variables de entorno
+
+# En desarrollo
+
+load_dotenv()
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+
+# En producción, usar secrets
+
+# st.secrets()
+
 
 # Verificar si la clave API está presente
 if not key:
-    st.error("API Key no encontrada. Asegúrate de tener una variable 'OPENAI_API_KEY' en tu archivo .env")
+    st.error(
+        "API Key no encontrada. Asegúrate de tener una variable 'GROQ_API_KEY' en tu archivo .env"
+    )
     st.stop()
 
-# Configurar la clave API de OpenAI
-openai.api_key = key
+
+# ══════════════════════════════════
+#   CREAR EL CLIENTE DE GROQ
+# ══════════════════════════════════
+
+cliente = Groq(api_key=GROQ_API_KEY, timeout=30)
+
+# Errores 503 Service Unavailable', 'type': 'internal_server_error' pueden evitarse añadiendo un tiempo de espera en segundos para la respuesta
+
+# ══════════════════════════════════
+#  EMPEZAMOS...
+# ══════════════════════════════════
 
 # Título de la aplicación
 st.title("Transcripción de audio usando Whisper")
 
 # Subir el archivo de audio
-fichero_audio = st.file_uploader("Sube un fichero de audio", type=['mp3', 'wav', 'm4a'])
+fichero_audio = st.file_uploader("Sube un fichero de audio", type=["mp3", "wav", "m4a"])
 
 if fichero_audio:
     # Mostrar el reproductor de audio
@@ -24,11 +54,13 @@ if fichero_audio:
     # Botón para iniciar la transcripción
     if st.button("Transcribir Audio"):
         try:
-            with st.spinner('Transcribiendo el audio, por favor espera...'):
+            with st.spinner("Transcribiendo el audio, por favor espera..."):
                 # Realizar la transcripción utilizando Whisper
-                transcripcion = openai.audio.transcriptions.create(
-                    model='whisper-1',
-                    file=fichero_audio
+                transcripcion = cliente.audio.transcriptions.create(
+                    model="whisper-large-v3-turbo",  # Modelo para la transcripción
+                    prompt="Transcribe este texto que está en castellano",  # Opcional
+                    language="es",  # Opcional
+                    temperature=0.0,  # Opcional
                 )
 
             # Mostrar la transcripción
@@ -39,6 +71,4 @@ if fichero_audio:
             st.error(f"Error al transcribir el audio: {e}")
 
 
-
-# Según la información disponible, la API de Whisper tiene un costo de $0.006 por minuto de audio procesado.  Esto significa que, por ejemplo, transcribir 10 minutos de audio costaría aproximadamente $0.06.
 
